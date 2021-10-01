78c1d04b7b1347462b9f42f208a4e90251f7eda0
hand-burger <jtmcash5@gmail.com>
2021-10-01 18:43:03 -0400

    🤸 Changed vars and comments

--- src/cubic.py
+++ src/cubic.py
@@ -1,11 +1,14 @@
-import math
+import math  # For square root
 
 print('Enter cubic coefficients: ', end='')
+
 coefficients = list(map(int, input().split()))
+
 print(str(coefficients[0]) + 'x^3+(' + str(coefficients[1]) +
       ')x^2+(' + str(coefficients[2]) + ')x+(' + str(coefficients[3]) + ')')
+# ax^3+bx^2+cx+d
 
-# Get p and q, then p/q list
+# Get factors
 
 
 def factorize(num):
@@ -23,43 +26,46 @@ if q_factors == []:
 
 p_over_qs = [None]
 
-# Now looping over each thing to get p/q
-
+# Loop over each factor to get p/q
 
 for j in range(len(p_factors)):
     for k in range(len(q_factors)):
         p_over_qs.append(p_factors[j]/q_factors[k])
 
+# Add p factors to the p/qs and remove the None at the start
 for i in p_factors:
     p_over_qs.append(i)
 p_over_qs.pop(0)
 
-# Alright now we got the p/qs, now test 'em, but first add the negatives
+# Alright, now we got the p/qs, now test 'em, but first add the negatives
 
 for i in range(len(p_over_qs)):
     p_over_qs.append(p_over_qs[i] * -1)
 
 
 for i in p_over_qs:
+    # Factor theory (valid factor (x-k), iff p(k) = 0)
     if (coefficients[0] * i * i * i)+(coefficients[1] * i * i)+(coefficients[2] * i) + (coefficients[3]) == 0:
         print('First root found:', i)
+
         # Now synthetic division for quadratic
 
+        # Quadratic coefficients
         synthetic1 = coefficients[0]
         synthetic2 = synthetic1 * i + coefficients[1]
         synthetic3 = synthetic2 * i + coefficients[2]
         synthetic4 = synthetic3 * i + coefficients[3]
 
-        # Now quadratic equation for the quadratic
-
         print(str(synthetic1) + 'x^2+(' + str(synthetic2) +
               ')x+(' + str(synthetic3) + ')')
 
-        d = synthetic2**2-4*synthetic1*synthetic3  # discriminant
+        # Now quadratic equation for the quadratic
 
-        if d < 0:
+        discriminant = synthetic2**2-4*synthetic1*synthetic3
+
+        if discriminant < 0:
             print("This quadratic has no real solution")
-        elif d == 0:
+        elif discriminant == 0:
             x = (-synthetic2 + math.sqrt(synthetic2**2 - 4 *
                  synthetic1*synthetic3)) / (2 * synthetic1)
             print("This quadratic has one solutions: ", x)
@@ -72,4 +78,7 @@ for i in p_over_qs:
 
         print('The roots are ' + str(x1) + ', ' + str(x2) + ', ' + str(i))
 
+        break
+    else:
+        print('No root found using p and q')
         break
