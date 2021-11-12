097ed31673f185b918ea83c44872c69f816a472c
hand-burger <jtmcash5@gmail.com>
2021-11-11 22:46:49 -0500

    🥩 Cv

--- src/OpenCV/test_webcam.py
+++ src/OpenCV/test_webcam.py
@@ -1,24 +1,27 @@
-import cv2  # OpenCv
+import cv2
 
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(
+    '/Users/jack/Documents/ICS4U/src/OpenCV/hand.xml')
+# To capture video from webcam..
 cap = cv2.VideoCapture(0)
-
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-
-
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
 while True:
-    ret, frame = cap.read()
-
-    # Get the frame from the webcam
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5,
-                       interpolation=cv2.INTER_AREA)
-    # Display the frame
-    cv2.imshow('Input', frame)
-
-    c = cv2.waitKey(1)
-    if c == 27:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
-
+# Release the VideoCapture object
 cap.release()
-cv2.destroyAllWindows()
